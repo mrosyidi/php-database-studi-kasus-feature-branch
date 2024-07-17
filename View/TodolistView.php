d8e6e88d6106b92e1f6d62c9deec7919dd471cf5
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-17 10:30:58 +0700

    update TodolistView.php menampilkan todolist

--- View/TodolistView.php
+++ View/TodolistView.php
@@ -3,6 +3,7 @@
   namespace View
   {
     use Service\TodolistService;
+    use Helper\InputHelper;
 
     class TodolistView
     {
@@ -11,6 +12,36 @@
       public function __construct(TodolistService $todolistService)
       {
         $this->todolistService = $todolistService;
+      }
+
+      public function showTodolist(): void
+      {
+        while(true)
+        {
+          $this->todolistService->showTodolist();
+          echo "MENU" . PHP_EOL;
+          echo "1. Tambah Todo" . PHP_EOL;
+          echo "2. Hapus Todo" . PHP_EOL;
+          echo "x. Keluar" . PHP_EOL;
+
+          $pilihan = InputHelper::input("pilih");
+
+          if($pilihan == "1")
+          {
+
+          }else if($pilihan == "2")
+          {
+
+          }else if($pilihan == "x")
+          {
+            break;
+          }else
+          {
+            echo "Pilihan tidak dimengerti" . PHP_EOL;
+          }
+        }
+
+        echo "Sampai Jumpa Lagi" . PHP_EOL;
       }
     }
   }
