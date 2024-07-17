782722b7e06880aa5ead360be386ac24df37afcb
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-07-17 10:52:45 +0700

    add TodolistViewTest.php

---
+++ Test/TodolistViewTest.php
@@ -0,0 +1,25 @@
+<?php
+
+  require_once __DIR__ . "/../Config/Database.php";
+  require_once __DIR__ . "/../Entity/Todolist.php";
+  require_once __DIR__ . "/../Repository/TodolistRepository.php";
+  require_once __DIR__ . "/../Service/TodolistService.php";
+  require_once __DIR__ . "/../View/TodolistView.php";
+  require_once __DIR__ . "/../Helper/InputHelper.php";
+
+  use Config\Database;
+  use Repository\TodolistRepositoryImpl;
+  use Service\TodolistServiceImpl;
+  use View\TodolistView;
+  use Helper\InputHelper;
+
+  function testViewShowTodolist(): void
+  {
+    $connection = Database::getConnection();
+    $todolistRepository = new TodolistRepositoryImpl($connection);
+    $todolistService = new TodolistServiceImpl($todolistRepository);
+    $todolistView = new TodolistView($todolistService);
+    $todolistView->showTodolist();
+  }
+
+  testViewShowTodolist();
